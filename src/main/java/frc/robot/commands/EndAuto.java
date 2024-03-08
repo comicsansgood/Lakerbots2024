0fc3064a0186f83000ad7e08c0d33f7b32eac1b0
comicsansgood <masonman0810@gmail.com>
2024-03-07 19:41:29 -0500

    added EndAuto

---
+++ src/main/java/frc/robot/commands/EndAuto.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LauncherSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class EndAuto extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  
+  
+  private final LauncherSubsystem launcher;
+  private final IntakeSubsystem intake;
+  private final FeederSubsystem feeder;
+  
+
+  
+  public EndAuto(LauncherSubsystem launcher, IntakeSubsystem intake, FeederSubsystem feeder) {
+    this.launcher = launcher;
+    this.intake = intake;
+    this.feeder = feeder;
+    addRequirements(launcher, intake, feeder);
+  }
+
+  @Override
+  public void initialize() {
+    launcher.launcherStop();
+    feeder.feederStop();
+    intake.intakeStop();
+    launcher.launcherAim(0);
+    intake.intakeIn();
+  }
+
+  @Override
+  public void execute() {
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
